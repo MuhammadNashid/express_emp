6dd70ebe7bc4c9a1610b73edb01c4225872364b6
harikrisshna0001 <muhammadnashid@gmail.com>
2024-10-15 13:44:26 +0530

    uploaded

---
+++ express_emp/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import Router from "./router.js";
+import connection from "./connection.js";
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('frontend'));  
+app.use(express.json());
+app.use('/api', Router);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('frontend/pages/index.html'));  
+});
+
+// Connect to the database and start the server
+connection().then(() => {
+    app.listen(port, () => {
+        console.log(`server started at http://localhost:${port}`);
+    });
+}).catch((error) => {
+    console.log(error);
+});
